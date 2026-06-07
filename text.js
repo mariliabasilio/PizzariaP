b2e2b2f0b3ee33f3341616809b9236d27f452430
marilia basilio <mariliabasilio4@gmail.com>
2026-06-07 14:51:05 -0300

    Primeira versão do site da pizzaria

---
+++ text.js
@@ -0,0 +1,47 @@
+const listaCarrinho = document.getElementById("lista-carrinho");
+const totalSpan = document.getElementById("total");
+
+let carrinho = [];
+let total = 0;
+
+function adicionarCarrinho(nome, preco){
+
+    carrinho.push({
+        nome,
+        preco
+    });
+
+    total += preco;
+
+    atualizarCarrinho();
+}
+
+function removerItem(index){
+
+    total -= carrinho[index].preco;
+
+    carrinho.splice(index, 1);
+
+    atualizarCarrinho();
+}
+
+function atualizarCarrinho(){
+
+    listaCarrinho.innerHTML = "";
+
+    carrinho.forEach((item, index) => {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            ${item.nome} - R$ ${item.preco.toFixed(2)}
+            <button onclick="removerItem(${index})">
+                X
+            </button>
+        `;
+
+        listaCarrinho.appendChild(li);
+    });
+
+    totalSpan.textContent = total.toFixed(2);
+}

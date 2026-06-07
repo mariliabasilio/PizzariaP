988d4a77f2bb2ca8e0000767b75105003eadc143
marilia basilio <mariliabasilio4@gmail.com>
2026-06-07 15:39:10 -0300

    Funcionalidade: Adição de carrinho de compras com inclusão e remoção de itens.

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ const totalSpan = document.getElementById("total");
 let carrinho = [];
 let total = 0;
 
-function adicionarCarrinho(nome, preco){
-
+function adicionarCarrinho(nome, preco) {
     carrinho.push({
-        nome,
-        preco
+        nome: nome,
+        preco: preco
     });
 
     total += preco;
@@ -16,8 +15,7 @@ function adicionarCarrinho(nome, preco){
     atualizarCarrinho();
 }
 
-function removerItem(index){
-
+function removerItem(index) {
     total -= carrinho[index].preco;
 
     carrinho.splice(index, 1);
@@ -25,19 +23,15 @@ function removerItem(index){
     atualizarCarrinho();
 }
 
-function atualizarCarrinho(){
-
+function atualizarCarrinho() {
     listaCarrinho.innerHTML = "";
 
     carrinho.forEach((item, index) => {
-
         const li = document.createElement("li");
 
         li.innerHTML = `
             ${item.nome} - R$ ${item.preco.toFixed(2)}
-            <button onclick="removerItem(${index})">
-                X
-            </button>
+            <button onclick="removerItem(${index})">X</button>
         `;
 
         listaCarrinho.appendChild(li);

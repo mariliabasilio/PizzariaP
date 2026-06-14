2c07d6420ac51dc29f6ff2c605fbba5cb400f0cc
marilia basilio <mariliabasilio4@gmail.com>
2026-06-14 17:01:55 -0300

    Refatora o layout do cabeçalho, adiciona funcionalidade ao carrinho e melhora a exibição dos itens no painel

--- script.js
+++ script.js
@@ -1,45 +1,86 @@
-const listaCarrinho = document.getElementById("lista-carrinho");
-const totalSpan = document.getElementById("total");
+const btnCarrinho = document.getElementById("btnCarrinho");
+const painelCarrinho = document.getElementById("painelCarrinho");
+
+btnCarrinho.addEventListener("click", () => {
+    painelCarrinho.classList.toggle("ativo");
+});
 
 let carrinho = [];
 let total = 0;
+function adicionarCarrinho(nome, preco){
 
-function adicionarCarrinho(nome, preco) {
-    carrinho.push({
-        nome: nome,
-        preco: preco
-    });
+    const itemExistente = carrinho.find(item => item.nome === nome);
 
-    total += preco;
+    if(itemExistente){
+        itemExistente.quantidade++;
+    }else{
+        carrinho.push({
+            nome,
+            preco,
+            quantidade: 1
+        });
+    }
 
-    atualizarCarrinho();
+    calcularTotal();
 }
 
-function removerItem(index) {
-    total -= carrinho[index].preco;
+function atualizarCarrinho(){
 
-    carrinho.splice(index, 1);
+    const lista = document.getElementById("listaCarrinho");
 
-    atualizarCarrinho();
-}
-
-function atualizarCarrinho() {
-    listaCarrinho.innerHTML = "";
+    lista.innerHTML = "";
 
     carrinho.forEach((item, index) => {
-        const li = document.createElement("li");
 
-        li.innerHTML = `
-            ${item.nome} - R$ ${item.preco.toFixed(2)}
-            <button onclick="removerItem(${index})">X</button>
+        lista.innerHTML += `
+            <li class="item-carrinho">
+
+                <span>${item.nome}</span>
+
+                <div class="controle-qtd">
+                    <button onclick="diminuirQuantidade(${index})">-</button>
+
+                    <span>${item.quantidade}</span>
+
+                    <button onclick="aumentarQuantidade(${index})">+</button>
+                </div>
+
+            </li>
         `;
-
-        listaCarrinho.appendChild(li);
     });
 
-    totalSpan.textContent = total.toFixed(2);
+    document.getElementById("total").textContent = total.toFixed(2);
+    document.getElementById("quantidade").textContent = carrinho.length;
 }
 
+function aumentarQuantidade(index){
+
+    carrinho[index].quantidade++;
+
+    calcularTotal();
+}
+
+function diminuirQuantidade(index){
+
+    carrinho[index].quantidade--;
+
+    if(carrinho[index].quantidade <= 0){
+        carrinho.splice(index, 1);
+    }
+
+    calcularTotal();
+}
+
+function calcularTotal(){
+
+    total = 0;
+
+    carrinho.forEach(item => {
+        total += item.preco * item.quantidade;
+    });
+
+    atualizarCarrinho();
+}
 // cadastro fechar e abrir overlay joão 
 
 const overlay = document.getElementById("overlay");
